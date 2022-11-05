220adb746f7e9abc0a58bf87700d679541ba3f19
hope1021 <kdy102130@naver.com>
2022-11-05 22:01:52 +0900

    <Feat> : "추가 [1-2] 랜덤 숫자 생성 "
    [1] 게임시작
    1-2. 랜덤 숫자 생성
    
    > 상세내용
    
    - MissionUtils 라이브러리의 Random.pickNumberInRange() 활용
    - 랜덤으로 발생되는 숫자는 1부터 9까지의 숫자를 반환하게 하였음.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,17 @@ class App {
     // [1] 게임 시작
     // 1-1. 게임 시작 문구 출력
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    
+
+    // 1-2. 랜덤 숫자 생성
+    while (computer.length < 3){
+      // computer의 길이가 3미만이면 문장을 수행하게 함.
+
+      random_number_computer = MissionUtils.Random.pickNumberInRange(1,9);
+      // 테스트케이스에 들어올 랜덤 숫자는  MissionUtils 라이브러리의 Random.pickNumberInRange()을 활용하여 1~9범위의 숫자를 반환
+      // * pickNumberInRange(startInclusive, endInclusive) : 숫자 범위를 지정하면 시작 또는 끝 숫자를 포함하여 범위의 숫자를 반환
+      
+    } // while
+
   }
 }
 

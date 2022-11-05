4a7e0f6a614b00b3d692f630e771fb094007e692
hope1021 <kdy102130@naver.com>
2022-11-05 21:37:31 +0900

    <Feat> : "완료 [0] 준비 단계"
    # 구현된 기능
    
    [0] 준비
    0-1. MissionUtils 라이브러리 모듈 설치 및 유틸 사용
    0-2. 변수 및 함수 선언
    
    > 상세내용
    
    - 테스트 패키지 설치 완료
    - Mission-Utils 라이브러리 설치 완료
    - 변수 및 함수 선언 `추가 예정`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
-  play() {}
+  play() {
+
+    // [0] 준비단계
+    // 0-1. MissionUtils 라이브러리 (CommonJS 방식)
+    const MissionUtils = require("@woowacourse/mission-utils");
+    
+    // 0-2.변수 및 함수 선언
+    const computer=[];             
+    let random_number_computer=[]; // 라이브러리로 생성된 랜덤한 숫자
+    let input_number_player=[];    // 정제 전 (type : object)
+    let clean_number_player=[];    // 정제 후 (type : number)
+    let player=[];       
+    
+  }
 }
 
 module.exports = App;
